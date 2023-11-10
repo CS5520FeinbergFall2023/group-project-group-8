44d1fe2b55262fc9aad363f67cdd8a43f557fbfb
cgormley1275 <cgormley1275@gmail.com>
2023-11-10 08:01:38 -0500

    Integrated project with Firebase Realtime Database

--- app/src/main/java/edu/northeastern/group_project_group_8/LoginActivity.java
+++ app/src/main/java/edu/northeastern/group_project_group_8/LoginActivity.java
@@ -1,14 +1,25 @@
 package edu.northeastern.group_project_group_8;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+
     }
 }
