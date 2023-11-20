9b66aa4c7591f9ab83f76de99008fdc0f5dd1e31
Chris <cgormley1275@gmail.com>
2023-11-19 19:14:20 -0500

    added graph that graphs portfolio value by date

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-analytics")
 
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
--- app/src/main/java/edu/northeastern/group_project_group_8/Dashboard.java
+++ app/src/main/java/edu/northeastern/group_project_group_8/Dashboard.java
@@ -4,13 +4,23 @@ import static java.lang.Thread.sleep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -52,50 +62,44 @@ public class Dashboard extends AppCompatActivity {
         }
 
         // This code is just to test out the graphing functionality
-//        lineChart = findViewById(R.id.lineChart);
-//        Description description = new Description();
-//        description.setText("Student Record");
-//        description.setPosition(150f, 15f);
-//        lineChart.setDescription(description);
-//        lineChart.getAxisRight().setDrawLabels(false);
-//
-//        xValues = Arrays.asList("Nadun","Kamal", "Jhon", "Jerry");
-//
-//        XAxis xAxis = lineChart.getXAxis();
-//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-//        xAxis.setValueFormatter(new IndexAxisValueFormatter(xValues));
-//        xAxis.setLabelCount(4);
-//        xAxis.setGranularity(1f);
-//
-//        YAxis yAxis = lineChart.getAxisLeft();
-//        yAxis.setAxisMinimum(0f);
-//        yAxis.setAxisMaximum(100f);
-//        yAxis.setAxisLineWidth(2f);
-//        yAxis.setAxisLineColor(Color.BLACK);
-//        yAxis.setLabelCount(10);
-//
-//        List<Entry> entries1 = new ArrayList<>();
-//        entries1.add(new Entry(0, 10f));
-//        entries1.add(new Entry(1, 10f));
-//        entries1.add(new Entry(2, 65f));
-//        entries1.add(new Entry(3, 45f));
-//
-//        List<Entry> entries2 = new ArrayList<>();
-//        entries2.add(new Entry(0, 5f));
-//        entries2.add(new Entry(1, 15f));
-//        entries2.add(new Entry(2, 25f));
-//        entries2.add(new Entry(3, 30f));
-//
-//        LineDataSet dataSet1 = new LineDataSet(entries1, "Math");
-//        dataSet1.setColor(Color.BLUE);
-//
-//        LineDataSet dataSet2 = new LineDataSet(entries2, "Science");
-//        dataSet2.setColor(Color.RED);
-//
-//        LineData lineData = new LineData(dataSet1, dataSet2);
-//
-//        lineChart.setData(lineData);
-//
-//        lineChart.invalidate();
+        lineChart = findViewById(R.id.lineChart);
+        Description description = new Description();
+        description.setText("Portfolio Value");
+        description.setPosition(250f, 150f);
+        lineChart.setDescription(description);
+        lineChart.getAxisRight().setDrawLabels(false);
+        ArrayList<LocalDate> portfolioDates = new ArrayList<>();
+        for (Price date: portfolioData.priceSumsByDate) {
+            portfolioDates.add(date.date);
+        }
+        xValues = Arrays.asList(String.valueOf(portfolioDates));
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xValues));
+        xAxis.setLabelCount(portfolioDates.size());
+        xAxis.setGranularity(1f);
+
+        YAxis yAxis = lineChart.getAxisLeft();
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(500f);
+        yAxis.setAxisLineWidth(2f);
+        yAxis.setAxisLineColor(Color.BLACK);
+        yAxis.setLabelCount(10);
+
+        List<Entry> entries1 = new ArrayList<>();
+        for (int i = 0; i < portfolioData.priceSumsByDate.size(); i++) {
+            Price currentPrice = portfolioData.priceSumsByDate.get(i);
+            entries1.add(new Entry(i, currentPrice.price.floatValue()));
+        }
+
+        LineDataSet dataSet1 = new LineDataSet(entries1, "Math");
+        dataSet1.setColor(Color.BLUE);
+
+        LineData lineData = new LineData(dataSet1);
+
+        lineChart.setData(lineData);
+
+        lineChart.invalidate();
     }
 }
